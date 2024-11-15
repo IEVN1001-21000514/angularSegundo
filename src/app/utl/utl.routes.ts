0cea949a3a868d5109ae1cf9037b663e3e64aa18
“Kevin <gomezyahirmtz2@gmail.com>
2024-11-14 21:01:43 -0600

    alumnos final 14/11/24

--- src/app/utl/utl.routes.ts
+++ src/app/utl/utl.routes.ts
@@ -9,7 +9,14 @@ export default[
     path: 'agregar',
     loadComponent:()=> import('./agregar/agregar.component')
 },
-
+{
+    path: 'eliminar/:matricula',
+    loadComponent:()=> import('./eliminar/eliminar.component')
+},
+{
+    path:'editar/:matricula',
+    loadComponent:()=> import('./editar/editar.component')
+}
 
 
 ] as Routes
